ccade769a51538735738c4cbab2a5cf57e415efe
Sam Shi <samrshi@outlook.com>
2024-10-12 19:59:02 -0400

    Reformat error init calls

--- Sources/HammockMacros/Stubbable/StubbableMacro.swift
+++ Sources/HammockMacros/Stubbable/StubbableMacro.swift
@@ -20,22 +20,34 @@ struct StubbableMacro: MemberAttributeMacro {
 extension StubbableMacro {
     private static func ensureCorrectApplication(macroSyntax: AttributeSyntax, declaration: some DeclGroupSyntax) throws {
         if declaration.is(ExtensionDeclSyntax.self) {
-            throw DiagnosticsError(syntax: macroSyntax, message: "'@Stubbable cannot be applied to extension", id: .invalidApplication)
+            throw DiagnosticsError(
+                syntax: macroSyntax,
+                message: "'@Stubbable cannot be applied to extension",
+                id: .invalidApplication)
         }
         
         guard let identified = declaration.asProtocol(NamedDeclSyntax.self) else {
-            throw DiagnosticsError(syntax: macroSyntax, message: "'@Stubbable' must be applied to a named type", id: .invalidApplication)
+            throw DiagnosticsError(
+                syntax: macroSyntax,
+                message: "'@Stubbable' must be applied to a named type",
+                id: .invalidApplication)
         }
         
         let stubbableTypeName = identified.name.trimmed
         
         
         if declaration.is(EnumDeclSyntax.self) {
-            throw DiagnosticsError(syntax: macroSyntax, message: "'@Stubbable' cannot be applied to enum \(stubbableTypeName)", id: .invalidApplication)
+            throw DiagnosticsError(
+                syntax: macroSyntax,
+                message: "'@Stubbable' cannot be applied to enum \(stubbableTypeName)",
+                id: .invalidApplication)
         }
         
         if declaration.is(ProtocolDeclSyntax.self) {
-            throw DiagnosticsError(syntax: macroSyntax, message: "'@Stubbable' cannot be applied to protocol \(stubbableTypeName)", id: .invalidApplication)
+            throw DiagnosticsError(
+                syntax: macroSyntax,
+                message: "'@Stubbable' cannot be applied to protocol \(stubbableTypeName)",
+                id: .invalidApplication)
         }
     }
 }
